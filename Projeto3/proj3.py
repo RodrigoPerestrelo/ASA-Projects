ac4a9c5fdb52dfe991012de5b23bc4cbcf1cf47a
Rodrigo Salvador dos Santos Perestrelo <rodrigo.perestrelo@tecnico.ulisboa.pt>
2024-01-07 11:45:31 +0000

    small_changes

--- Projeto3/proj3.py
+++ Projeto3/proj3.py
@@ -1,8 +1,5 @@
 from pulp import LpMaximize, LpProblem, LpVariable, value, GLPK, LpInteger, lpSum
 
-funcao_objetivo = 0
-restricao_1 = 0
-
 # Criação do problema
 problema = LpProblem(name="Maximizar_Lucro", sense=LpMaximize)
 
@@ -13,7 +10,7 @@ brinquedos = [None] * n
 restriction_1 = [None] * n
 objective_function = [None] * n
 capacidade_producao_lista =  [None] * n
-teste = [None] * n
+vetor_pacotes = [None] * n
 
 # Brinquedos
 for i in range(n):
@@ -29,36 +26,37 @@ for i in range(n):
 # Pacotes
 for j in range(p):
     x, y, z, lucro = map(int, input().split())
-    toys1 = capacidade_producao_lista[x-1]
-    toys2 = capacidade_producao_lista[y-1]      
-    toys3 = capacidade_producao_lista[z-1]      
-    zecarlos = min(toys1, toys2, toys3)
-    aux = LpVariable(name=f"pacote_{j}", lowBound=0, upBound=zecarlos, cat=LpInteger)
+    b1 = capacidade_producao_lista[x-1]
+    b2 = capacidade_producao_lista[y-1]      
+    b3 = capacidade_producao_lista[z-1]      
+    up = min(b1, b2, b3)
+    aux = LpVariable(name=f"pacote_{j}", lowBound=0, upBound=up, cat=LpInteger)
 
-    if teste[x-1] != None:
-        teste[x-1].append(aux)
+    if vetor_pacotes[x-1] != None:
+        vetor_pacotes[x-1].append(aux)
     else:
-        teste[x-1] = [aux]
-    if teste[y-1] != None:
-        teste[y-1].append(aux)
+        vetor_pacotes[x-1] = [aux]
+    if vetor_pacotes[y-1] != None:
+        vetor_pacotes[y-1].append(aux)
     else:
-        teste[y-1] = [aux]
-    if teste[z-1] != None:
-        teste[z-1].append(aux)
+        vetor_pacotes[y-1] = [aux]
+    if vetor_pacotes[z-1] != None:
+        vetor_pacotes[z-1].append(aux)
     else:
-        teste[z-1] = [aux]
+        vetor_pacotes[z-1] = [aux]
 
     objective_function[j] += (aux * lucro)
     restriction_1[j] += aux*3
 
 # Adição da função objetivo
 problema += lpSum(objective_function)
+
 # Adição das restrições
 problema += lpSum(restriction_1) <= max_brinquedos, "Restricao_1"
 
 for i in range(n):
-    if(teste[i] != None):
-        problema += lpSum(teste[i]) + brinquedos[i] <= capacidade_producao_lista[i], f"Restricao_{i + 2}"
+    if(vetor_pacotes[i] != None):
+        problema += lpSum(vetor_pacotes[i]) + brinquedos[i] <= capacidade_producao_lista[i], f"Restricao_{i + 2}"
 
 # Resolução do problema
 problema.solve(GLPK(msg=0))
